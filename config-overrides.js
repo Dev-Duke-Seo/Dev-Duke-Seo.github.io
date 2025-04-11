7c9519f67c355fb9b8c636160f632788618aebac
Dev-Duke-Seo <dev.duke.seo@gmail.com>
2025-04-11 19:22:35 +0900

    feat: 의존성 추가, tsconfig 설정 변경 및 경로 별칭 추가

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,18 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
+
 module.exports = function override(config, env) {
   // Node.js 폴리필 설정
   config.resolve.fallback = {
     ...config.resolve.fallback,
     "path": require.resolve("path-browserify"),
     "fs": require.resolve("browserify-fs")
+  };
+  
+  // Path alias 설정 추가
+  config.resolve.alias = {
+    ...config.resolve.alias,
+    '@': path.resolve(__dirname, 'src'),
   };
   
   return config;
