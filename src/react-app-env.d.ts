f83f362f3fbe2c2ebfdb9858acfa06c89d502edc
Dev-Duke-Seo <dev.duke.seo@gmail.com>
2025-04-11 19:30:39 +0900

    feat: react-syntax-highlighter 모듈 선언을 위한 타입 정의 추가

---
+++ src/react-app-env.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="react-scripts" />
+
+// react-syntax-highlighter 모듈 선언
+declare module 'react-syntax-highlighter';
+declare module 'react-syntax-highlighter/dist/esm/styles/hljs';
